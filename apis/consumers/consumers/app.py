45ff2173a52fa9f93385ec9c82f3719b295eab0f
Rendell Arruda <rendellarruda3@gmail.com>
2024-09-23 06:37:30 -0300

    create functions

--- apis/consumers/consumers/app.py
+++ apis/consumers/consumers/app.py
@@ -13,13 +13,45 @@ users = {
 companies = {
                 "companies": [
                     {"name": "empresa1", "telefone": "47999999999"},
-                    {"name": "empresa2", "telefone": "47999999999"},
-                    {"name": "empresa3", "telefone": "47999999999"},
+                    {"name": "empresa2", "telefone": "37999999999"},
+                    {"name": "empresa3", "telefone": "27999999999"},
                 ]
             }
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/consumers/person', methods=['POST'])
+def create_user():
+    # 
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuário {requests_params} criado com sucesso!"
+    }
+    return response
+
+@app.rout('/consumers/person', methods=['PUT'])
+def update_user():
+    resquest_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuário {resquest_params} atualizado com sucesso!"    
+    }
+    return response
+
+@app.route('/consumers/person', methods=['DELETE'])
+def delete_user():
+    resquest_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuário {resquest_params} deletado com sucesso!"    
+    }
+    return response
+
+@app.route('/consumers/person', methods=['GET'])
+def get_person():
+    response = {
+        "statusCode": 200,
+        "body": users
+    }
+    return response
 
